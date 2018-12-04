3155ae0b99950245fdcc67d5a2e4f805dad9d52d
Eduardo Aguad <eaguad@meat.cl>
2018-12-03 22:21:08 -0300

    Customizations

--- src/layouts.js
+++ src/layouts.js
@@ -4,14 +4,14 @@ module.exports = {
 
 		_meta: {
 			"tab": { key: "\t", text: "Tab", width: 60, classes: "control"},
-			"shiftl": { keySet: "shifted", text: "Shift", width: 100, classes: "control"},
-			"shiftr": { keySet: "shifted", text: "Shift", width: 100, classes: "control"},
-			"caps": { keySet: "capsed", text: "Caps lock", width: 80, classes: "control"},
-			"space": { key: " ", text: "Space", width: 180},
+			"shiftl": { keySet: "shifted", text: "Mayús", width: 100, classes: "control"},
+			"shiftr": { keySet: "shifted", text: "Mayús", width: 100, classes: "control"},
+			"caps": { keySet: "capsed", text: "Mayús lock", width: 80, classes: "control"},
+			"space": { key: " ", text: "Espacio", width: 180},
 			"enter": { key: "\r\n", text: "Enter", width: 80, classes: "control"},
 			"backspace": { func: "backspace", classes: "control backspace", width: 65},
-			"accept": { func: "accept", text: "Close", classes: "control featured"},
-			"next": { func: "next", text: "Next", classes: "control featured"}
+			"accept": { func: "accept", text: "Cerrar", classes: "control featured"},
+			"next": { func: "next", text: "Siguiente", classes: "control featured"}
 		},
 
 		default: [
@@ -45,25 +45,27 @@ module.exports = {
 			"alpha": { keySet: "default", text: "Abc", classes: "control"},
 			"shift": { keySet: "shifted", text: "ABC", classes: "control"},
 			"numbers": { keySet: "numbers", text: "123", classes: "control"},
-			"space": { key: " ", text: "Space", width: 200},
+			"space": { key: " ", text: "Espacio", width: 200},
 			"backspace": { func: "backspace", classes: "control"},
-			"accept": { func: "accept", text: "Close", classes: "control featured"},
-			"next": { func: "next", text: "Next", classes: "featured"},
+			"accept": { func: "accept", text: "Cerrar", classes: "control featured"},
+			"next": { func: "next", text: "Siguiente", classes: "featured"},
 			"zero": { key: "0", width: 130}
 		},
 
 		default: [
+			"@ 1 2 3 4 5 6 7 8 9 0 {backspace}",
 			"q w e r t y u i o p",
 			" a s d f g h j k l ",
-			"{shift} z x c v b n m {backspace}",
-			"{numbers} , {space} . {next} {accept}"
+			"{shift} z x c v b n m",
+			"{numbers} , {space} . {accept}"
 		],
 
 		shifted: [
+			"@ 1 2 3 4 5 6 7 8 9 0 {backspace}",
 			"Q W E R T Y U I O P",
 			" A S D F G H J K L ",
 			"{default} Z X C V B N M ",
-			"{numbers} _ {space} {backspace} {next} {accept}"
+			"{numbers} _ {space} {accept}"
 		],
 
 		numbers: [
